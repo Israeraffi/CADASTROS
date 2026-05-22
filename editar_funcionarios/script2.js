521a6fabe37e8ab14085449bef517960eba146db
israel <israelraffi571k@gmail.com>
2026-05-22 13:10:55 -0300

    atualizand_pt2

--- editar_funcionarios/script2.js
+++ editar_funcionarios/script2.js
@@ -1,22 +1,40 @@
-const users = JSON.parse(localStorage.getItem("users")
-  ) || [];
+const users = JSON.parse(localStorage.getItem("users")) || [];
 const pesquisar = document.getElementById("pesquisar");
 const fund_tela = document.getElementById("fundo-tela");
 const pesquisar_pes = document.getElementById("name");
 const pes_name = document.getElementById("pes_name");
-const bloco = document.getElementById("p_users")
+const bloco = document.getElementById("p_users");
 
-if (users.length === 0){
+if (users.length === 0) {
   bloco.style.display = "none";
-  
-} 
+}
 
 function renderUsers(lista) {
-  document.getElementById("p_users").textContent = JSON.stringify(
-    lista,
-    null,
-    2,
-  );
+  bloco.innerHTML = "";
+
+  lista.forEach((user) => {
+    bloco.innerHTML += `
+      <p class="table">
+        [
+          {
+            Id: ${user.id}, 
+            Funcionário: ${user.name},
+            E-mail: ${user.email}, 
+            Idade: ${user.idade},
+            Cargo: ${user.cargo}
+          }
+        ]
+
+        <button class="editar_user">
+          Editar
+        </button>   <button class="excluir_user">
+          Excluir
+        </button> 
+
+        
+      </p>
+    `;
+  });
 }
 
 renderUsers(users);
@@ -30,4 +48,3 @@ pesquisar_pes.addEventListener("input", (e) => {
 
   renderUsers(filteredUsers);
 });
-
